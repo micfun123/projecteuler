f5567d044c924d0880251344a82813d695b99c26
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-03 14:51:04 +0000

    9

---
+++ 9.py
@@ -0,0 +1,19 @@
+a = 1
+b = 1
+c = 1
+
+
+for i in range(1,500):
+    for j in range(1,500):
+        for k in range(1,500):
+            if i + j + k == 1000:
+                if i**2 + j**2 == k**2:
+                    a = i
+                    b = j
+                    c = k
+print(a*b*c)
+print(a)
+print(b)
+print(c)
+
+                    
