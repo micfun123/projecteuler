aa590b470b8f4ed5c73bb842166ef1af48baa65f
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-03 14:52:43 +0000

    lower range

--- 9.py
+++ 9.py
@@ -2,10 +2,9 @@ a = 1
 b = 1
 c = 1
 
-
-for i in range(1,500):
-    for j in range(1,500):
-        for k in range(1,500):
+for i in range(100,500):
+    for j in range(100,500):
+        for k in range(100,500):
             if i + j + k == 1000:
                 if i**2 + j**2 == k**2:
                     a = i
