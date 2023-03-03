601c786c12f4864eb3bac4333e99acd4aa2d04c2
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-03-03 14:59:06 +0000

    clean

--- 9.py
+++ 9.py
@@ -2,17 +2,12 @@ a = 1
 b = 1
 c = 1
 
-for i in range(100,500):
-    for j in range(100,500):
-        for k in range(100,500):
-            if i + j + k == 1000:
-                if i**2 + j**2 == k**2:
-                    a = i
-                    b = j
-                    c = k
-print(a*b*c)
-print(a)
-print(b)
-print(c)
-
+for a in range(100,500):
+    for b in range(100,500):
+        for c in range(100,500):
+            if a + b + c == 1000:
+                if a**2 + b**2 == c**2:
+                    print(a*b*c)
+                    print(a,b,c)
+                    break
                     
