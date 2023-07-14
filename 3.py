2c2381f8b53c8bbe571b6da038f39fccf8acabe7
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-07-14 07:36:17 +0100

    speed

--- 3.py
+++ 3.py
@@ -4,12 +4,24 @@
 #
 #What is the largest prime factor of the number 600851475143 ?
 
-number = int(input("Enter a number: "))
-primefactors = []
-for i in range(2, number):
-    print(i)
-    if number % i == 0:
-        primefactors.append(i)
-        number = number / i
+import math
 
-print(primefactors)
+def compute():
+	n = 600851475143
+	while True:
+		p = smallest_prime_factor(n)
+		if p < n:
+			n //= p
+		else:
+			return str(n)
+
+
+# Returns the smallest factor of n, which is in the range [2, n]. The result is always prime.
+def smallest_prime_factor(n):
+    assert n >= 2
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return i
+    return n  # n itself is prime
+
+print(compute())
